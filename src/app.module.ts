7383841b9bdb88dff3728b20240a50cb8af345db
Rafael Barbosa de Carvalho <rafaelbarbosadc@gmail.com>
2022-06-11 13:43:59 -0400

    Working API

---
+++ src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { PostService } from './post.service';
+import { PrismaService } from './prisma.service';
+import { UserService } from './user.service';
+
+@Module({
+  imports: [],
+  controllers: [AppController],
+  providers: [PrismaService, AppService, UserService, PostService],
+})
+export class AppModule {}
